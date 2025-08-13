648e123022115e1776e6c8d5f460931d14da4fbb
gugucazhiz <domateklh@gmail.com>
2025-08-13 00:20:09 -0300

    Update All Interfaces;still remaining=postgresql,remove option

---
+++ ptest/src/main/java/ptest/Responsavel.java
@@ -0,0 +1,29 @@
+package ptest;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class Responsavel {
+	@Id @GeneratedValue
+	private Long id;
+	private String nome;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
